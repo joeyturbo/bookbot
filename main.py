b7efc7a508f6fa5cb2698f85e5d0969645dd170f
joeyturbo <joeyturbo@gmail.com>
2024-12-28 14:58:06 -0500

    created get_char_count function to create dictionary of characters and the number of times they appear

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ def main():
   book_path = 'books/frankenstein.txt'
   text = get_book_text(book_path)
   num_words = get_word_count(text)
+  chars_dict = get_char_count(text)
+
   
 
 def get_book_text(path):
@@ -10,5 +12,15 @@ def get_book_text(path):
   
 def get_word_count(text):
   return len(text.split())
+
+def get_char_count(text):
+  chars = {}
+  for c in text:
+    lowered = c.lower()
+    if lowered in chars:
+      chars[lowered] += 1
+    else:
+      chars[lowered] = 1
+  return chars
 
 main()
