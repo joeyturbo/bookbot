51fe08d76042a16e96af89a001fc9ce5ab6cc9c6
joeyturbo <joeyturbo@gmail.com>
2024-12-28 14:57:01 -0500

    created get_word_count function

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 def main():
   book_path = 'books/frankenstein.txt'
   text = get_book_text(book_path)
+  num_words = get_word_count(text)
   
 
 def get_book_text(path):
   with open(path) as f:
     return f.read()
   
+def get_word_count(text):
+  return len(text.split())
 
 main()
